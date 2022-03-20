caa5b1de730d27607172e8f960532d71a2fbdf73
chriskimball <92878250+chriskimball@users.noreply.github.com>
2022-03-19 22:13:11 -0700

    Adjustments to models

--- models/User.js
+++ models/User.js
@@ -38,9 +38,7 @@ const userSchema = new Schema({
   ],
 });
 
-/*TODO:
-Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
-*/
+// Virtual called friendCount that retrieves the length of the user's friends array field on query.
 
 userSchema
     .virtual('friendCount')
