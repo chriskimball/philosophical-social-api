8a0ef3cfade526f22ac099bb6c2d794f00343e9b
chriskimball <92878250+chriskimball@users.noreply.github.com>
2022-03-19 22:36:45 -0700

    Added virtuals option to User model to allow for friend count virtual

--- models/User.js
+++ models/User.js
@@ -1,45 +1,50 @@
 const { Schema, model } = require("mongoose");
 
 // TODO:
-const userSchema = new Schema({
-  /* */
-  username: {
-    type: String,
-    required: [true, "Username is required!"],
-    unique: true,
-    trim: true,
-  },
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Username is required!"],
+      unique: true,
+      trim: true,
+    },
 
-  email: {
-    type: String,
-    required: [true, "Email address is required!"],
-    unique: true,
-    trim: true,
-    validate: {
-      validator: function (v) {
-        return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(v);
+    email: {
+      type: String,
+      required: [true, "Email address is required!"],
+      unique: true,
+      trim: true,
+      validate: {
+        validator: function (v) {
+          return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(v);
+        },
+        message: (props) => `${props.value} is not a valid email address!`,
       },
-      message: (props) => `${props.value} is not a valid email address!`,
     },
+
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
   },
-
-  thoughts: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Thought",
+  {
+    toJSON: {
+      virtuals: true,
     },
-  ],
-
-  friends: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-    },
-  ],
-});
+  }
+);
 
 // Virtual called friendCount that retrieves the length of the user's friends array field on query.
-
 userSchema
     .virtual('friendCount')
     .get(function () {
