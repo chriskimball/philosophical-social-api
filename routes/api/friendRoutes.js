5109425eb58f04c2778dd24b4ac421f899952620
chriskimball <92878250+chriskimball@users.noreply.github.com>
2022-03-19 22:52:18 -0700

    Removed commented out code from friendRoutes and userRoutes routers

--- routes/api/friendRoutes.js
+++ routes/api/friendRoutes.js
@@ -5,43 +5,6 @@ const {
   removeFriend,
 } = require('../../controllers/friendController');
 
-// TODO:
-// POST new friend by :friendId to user by :userId `/api/users/:userId/friends/:friendId`
 router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
-
-// router.post("/:userId/friends/:friendId", async (req, res) => {
-//   try {
-//       const result = await User.findOneAndUpdate(
-//         { _id: req.params.userId }, 
-//         {
-//           $addToSet: { friends: { _id: req.params.friendId } }
-//         },
-//         { runValidators: true, new: true }
-//       );
-
-//       res.status(200).json(result);
-//   } catch (err) {
-//     res.json(500).send(err);
-//   }
-// });
-
-// // TODO:
-// // DELETE friend by :friendId from user by :userId `/api/users/:userId/friends/:friendId`
-
-// router.delete("/:userId/friends/:friendId", async (req, res) => {
-//   try {
-//     const result = await User.findByIdAndUpdate(
-//       req.params.userId, 
-//       {
-//         $pull: { friends: { _id: req.params.friendId } }
-//       },
-//       { runValidators: true, new: true }
-//     );
-
-//     res.status(200).json(result);
-//   } catch (err) {
-//     res.json(500).send(err);
-//   }
-// });
 
 module.exports = router;
